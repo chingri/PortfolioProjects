9d25464ad8b7700015978f7cd793ce35e7fde21c
chingri <114172746+chingri@users.noreply.github.com>
2023-01-01 20:07:56 +0530

    Update Second_Witness.sql

--- SQL/SQL City Murder Mystery/Witness Testimonies/Second_Witness.sql
+++ SQL/SQL City Murder Mystery/Witness Testimonies/Second_Witness.sql
@@ -1,20 +1,24 @@
 --Lets look at Annabel Miller's side of the story 
-select*from person 
-where address_street_name = "Northwestern Dr"
-order by address_number desc  
+SELECT*
+FROM   person
+WHERE  address_street_name = "northwestern dr"
+ORDER  BY address_number DESC  
 -- Annabel Miller Info -- 
 -- id = 16371 , ssn = 318771143, license-id = 	490173-- 
 
 --Let's find some extra info on her 
-select * from facebook_event_checkin 
-where person_id = 16371
+SELECT *
+FROM   facebook_event_checkin
+WHERE  person_id = 16371 
 -- Annabel Miller attended "The Funky Grooves Tour" on the day of the murder
 
 --Let's take a look at her interview 
-select* from interview  
-where person_id =  16371 
+SELECT*
+FROM   interview
+WHERE  person_id = 16371  
 -- She recognized the killer from the gym from when she went to work out on the 9th of Jan 
 
 -- Lets look at the list of people who attended the gym on those days 
-select*from get_fit_now_check_in 
-where check_in_date = 20180109
+SELECT*
+FROM   get_fit_now_check_in
+WHERE  check_in_date = 20180109  
